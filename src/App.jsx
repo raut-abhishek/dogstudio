f8489d3222c3cd7f5069cbc7920f32ac6b104eff
raut-abhishek <iamabhishekraut01@gmail.com>
2026-01-19 20:38:53 +0530

    added sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,16 @@ function App() {
 
   return (
     <>
-    <Canvas>
-      <Dog/>
-    </Canvas>
+      <main>
+        <Canvas style={{height:"100vh",width:"100vw", position:"fixed", top:0, left:0, backgroundImage:"url(/background-l.png)", backgroundSize:"cover", backgroundRepeat:"-moz-repeat"}}>
+          <Dog/>
+        </Canvas>
+        <section id='section-1'></section>
+        <section id='section-2'></section>
+        <section id='section-3'></section>
+
+      </main>
+    
     </>
   )
 }
